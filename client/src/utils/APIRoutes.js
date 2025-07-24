9d01e57200d283d77cde167f3badbb33f36e2ed4
rkrustom <rajeevkumar85297@gmail.com>
2025-07-24 19:29:33 +0530

    chore: updated API host to use environment variable

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,5 +1,6 @@
 // export const host = "http://localhost:5000";
-export const host = "https://chatify-kkpf.onrender.com";
+// export const host = "https://chatify-kkpf.onrender.com";
+export const host = process.env.REACT_APP_API_HOST;
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
